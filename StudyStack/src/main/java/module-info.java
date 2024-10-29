6d66c3d66bb3f3d22594705cc1ef41390c06822a
EfeEryildiz <yigitefem3@gmail.com>
2024-10-29 12:48:19 -0400

    Added FirebaseConnection.java

--- StudyStack/src/main/java/module-info.java
+++ StudyStack/src/main/java/module-info.java
@@ -1,6 +1,13 @@
 module org.example.studystack {
     requires javafx.controls;
     requires javafx.fxml;
+    requires firebase.admin;
+    requires google.cloud.firestore;
+    requires com.google.auth.oauth2;
+    requires com.google.auth;
+    requires com.google.api.apicommon;
+    requires com.google.api.client;
+    requires com.google.api.client.auth;
 
 
     opens org.example.studystack to javafx.fxml;
---
+++ StudyStack/src/main/java/org/example/studystack/FirebaseConnection.java
@@ -0,0 +1,28 @@
+package org.example.studystack;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirebaseConnection {
+
+    public static void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("StudyStack/src/main/resources/org/example/studystack/key.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://capstone-team-4-e2207-default-rtdb.firebaseio.com")
+                    .build();
+
+            FirebaseApp firebaseApp;
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
